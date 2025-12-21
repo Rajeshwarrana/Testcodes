bc307b2de420189da2ff3f952c2a7843cc87bfcb
Rajeshwarrana <rajeshwarrana09@gmail.com>
2025-12-21 20:53:57 +0530

    Enumeration

---
+++ C++ codes/enumeration.cpp
@@ -0,0 +1,26 @@
+//Enum are the user-defined data type in C++ that consists of integral constants, and each of them is assigned a name.
+
+#include <iostream>
+
+enum Ranking{
+    Poor = 1,        // 0
+    Average,     // 1
+    Good ,        // 2
+    Excellent    // 3
+};
+
+int main(){
+
+    Ranking Player1 = Good;
+    Ranking Player2 = Excellent;
+    Ranking Player3 = Average;
+
+    std::cout << "Player 1 Ranking: " << Player1 << std::endl;
+    std::cout << "Player 2 Ranking: " << Player2 << std::endl;
+    std::cout << "Player 3 Ranking: " << Player3 << std::endl;
+    return 0;
+
+    
+}
+
+
